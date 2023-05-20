3aaaef5236a65dae28fe2567af11ad678d4f6f9a
hritick1 <80475837+hritick1@users.noreply.github.com>
2023-05-20 22:58:17 +0530

    Update axios.js

--- src/interceptors/axios.js
+++ src/interceptors/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 axios.defaults.baseURL="https://course-app.onrender.com";
 axios.interceptors.response.use(res=>res,async err=>{
-    if(err.response.status==400 ){
+    if(err.response.status==401){
         const response=await axios.get('/refresh');
 
         if(response.status==200){
